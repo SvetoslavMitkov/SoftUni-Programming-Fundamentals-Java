e64a5820330c2045241dc94e745d76aaa3060aba
Svetoslav Mitkov <svetlio3333.sm@gmail.com>
2022-09-29 17:38:29 +0300

    Arrays - tasks and useful things

---
+++ Arrays/NumbersInReversedOrder.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class NumbersInReversedOrder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        int[] numbersArr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int currentNumber = Integer.parseInt(scanner.nextLine());
+            numbersArr[i] = currentNumber;
+        }
+        for (int i = numbersArr.length - 1; i >= 0 ; i--) {
+            System.out.printf("%d ",numbersArr[i]);
+
+        }
+
+    }
+}
